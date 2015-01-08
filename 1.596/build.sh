80359f85374a18a6c728fc9aedd2063fdc5bad57
root <root@qp6.agileview.co.uk>
2015-01-08 10:12:36 +0000

    @Miles merging dind

---
+++ 1.596/build.sh
@@ -0,0 +1,13 @@
+JENKINS_HOME=/var/jenkins_home
+JENKINS_USER=jenkins
+JENKINS_DOCKER_IMAGE=ubuntu-jenkins-docker
+JENKINS_DOCKER_CONTAINER=$JENKINS_DOCKER_IMAGE
+rm -rf $JENKINS_HOME
+userdel $JENKINS_USER
+docker rm $JENKINS_DOCKER_CONTAINER
+
+docker build -t $JENKINS_DOCKER_IMAGE .  
+useradd -d "$JENKINS_HOME" -u 9765 -m -s /bin/bash $JENKINS_USER
+docker run --privileged -i -t --name $JENKINS_DOCKER_CONTAINER -p 9090:8080 -v $JENKINS_HOME:$JENKINS_HOME $JENKINS_DOCKER_IMAGE bash
+
+
